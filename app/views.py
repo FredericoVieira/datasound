1e4cbb0cf992f99708222602b0209de400be7fc1
FredericoVieira <fredops1@hotmail.com>
2016-11-29 16:48:16 -0200

    Updates views to handle with spotify login.

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from flask import Flask, redirect, url_for, session, request
+from flask import redirect, url_for, session, request
 from flask_oauthlib.client import OAuth, OAuthException
 from config import SPOTIFY_APP_ID, SPOTIFY_APP_SECRET, SECRET_KEY
 
@@ -8,19 +8,27 @@ app.secret_key = SECRET_KEY
 
 oauth = OAuth(app)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+@app.route('/score')
+def score():
+    return render_template('interpolate.html', user_id=request.args['user_id'], oauth_token=request.args['oauth_token'])
+
+
 spotify = oauth.remote_app('spotify',
-    consumer_key=SPOTIFY_APP_ID,
-    consumer_secret=SPOTIFY_APP_SECRET,
-    request_token_params={'scope': 'user-read-email'},
-    base_url='https://accounts.spotify.com',
-    request_token_url=None,
-    access_token_url='/api/token',
-    authorize_url='https://accounts.spotify.com/authorize'
-)
+                           consumer_key=SPOTIFY_APP_ID,
+                           consumer_secret=SPOTIFY_APP_SECRET,
+                           request_token_params={'scope': 'user-read-email'},
+                           base_url='https://accounts.spotify.com',
+                           request_token_url=None,
+                           access_token_url='/api/token',
+                           authorize_url='https://accounts.spotify.com/authorize'
+                           )
+
 
 @app.route('/login')
 def login():
@@ -34,7 +42,6 @@ def login():
 
 @app.route('/login/authorized')
 def spotify_authorized():
-    # import pdb; pdb.set_trace();
     resp = spotify.authorized_response()
     if resp is None:
         return 'Access denied: reason={0} error={1}'.format(
@@ -47,19 +54,19 @@ def spotify_authorized():
     session['oauth_token'] = (resp['access_token'], '')
     me = spotify.get('https://api.spotify.com/v1/me')
 
-    return 'Logged in as id={0}'.format(
-        me.data['id']
-    )
+    return redirect(url_for('score', user_id=me.data['id'], oauth_token=resp['access_token']))
 
 
 @spotify.tokengetter
 def get_spotify_oauth_token():
     return session.get('oauth_token')
 
+
 @app.route('/logout')
 def logout():
     session.pop('oauth_token', None)
     return redirect(url_for('index'))
+
 
 @spotify.tokengetter
 def get_spotify_oauth_token():
