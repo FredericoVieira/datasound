451e332ba30f5e2694f8c16f9eaad72f8103edb1
Frederico <fredops1@hotmail.com>
2016-12-04 11:46:05 -0200

    Updates flask port var to deploy on heroku.

--- manager.py
+++ manager.py
@@ -5,5 +5,5 @@ from flask_script import Server
 port = int(os.environ.get("PORT", 5000))
 server = Server(host="0.0.0.0", port=port)
 
-manager.add_command('runserver', Server())
+manager.add_command('runserver', server)
 manager.run()
